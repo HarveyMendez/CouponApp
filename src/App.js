25597c65d9c5901e3d985e3870d5f445c0d54634
HarveyMendez <harvey.mendez@ucr.ac.cr>
2024-06-06 10:38:08 -0600

    commit 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Agrega Navigate
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CompanyProfilePage from './pages/CompanyProfilePage';
 import LoginPage from './pages/LoginPage';
@@ -9,13 +9,11 @@ import { AuthProvider } from './context/AuthContext';
 const App = () => {
   return (
     <AuthProvider>
-      <Router>
+      <Router basename="/couponApp">
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          {/* Cambia la ruta de "/" para que apunte al componente de inicio de sesión */}
-          <Route path="/couponApp/login" element={<Navigate to="/login" />} />
           <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/dashboard" element={<PrivateRoute><HomePage /></PrivateRoute>} /> {/* Puedes cambiar esto si deseas */}
+          <Route path="/dashboard" element={<PrivateRoute><HomePage /></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><CompanyProfilePage /></PrivateRoute>} />
         </Routes>
       </Router>
